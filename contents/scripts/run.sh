a6d5cb609e99b6fabbd7714e6a0e5803ad3563e1
Alexandre Teles <alexandre.teles@ufba.br>
2023-01-21 17:36:29 -0300

    fix: fix paths

--- contents/scripts/run.sh
+++ contents/scripts/run.sh
@@ -21,8 +21,8 @@ wget https://api.purpurmc.org/v2/purpur/${MC_VERSION}/latest/download -O /app/se
 # Download the latest Geyser and Floodgate builds
 
 # If you want to use a specific version, you can replace the lastSuccessfulBuild with the build number
-wget https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/build/libs/Geyser-Spigot.jar -O ./server/plugins/Geyser-Spigot.jar
-wget https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/build/libs/floodgate-spigot.jar -O ./server/plugins/floodgate-spigot.jar
+wget https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/build/libs/Geyser-Spigot.jar -O /app/server/plugins/Geyser-Spigot.jar
+wget https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/build/libs/floodgate-spigot.jar -O /app/server/plugins/floodgate-spigot.jar
 
 # Run the server
 java --add-modules=jdk.incubator.vector ${JVM_SETTINGS} -jar /app/server/purpur.jar nogui
